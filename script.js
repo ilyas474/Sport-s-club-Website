d002074a6c331135bbe85276c0f685a8884e242e
ilyas474 <ilyasezrin@gmail.com>
2026-09-19 13:08:56 +0800

    added a search bar

--- script.js
+++ script.js
@@ -6,3 +6,32 @@ function scrollCoreValues(direction) {
         behavior: "smooth"
     });
 }
+
+function submitForm(event) {
+    event.preventDefault();
+    alert("Thank you for your enquiry!");
+    window.location.href="Submit-Enquiry.html";
+}
+
+function searchNews() {
+    let searchText = document.getElementById("searchBar").value.toLowerCase();
+    let newsCards = document.querySelectorAll(".news-card");
+    let searchResult = document.getElementById("searchResult");
+
+    if (searchText.trim() !== "") {
+        searchResult.textContent = 'Showing search results for "' + searchText + '":';
+    } else {
+        searchResult.textContent = "";
+    }
+
+    newsCards.forEach(function(card) {
+        let content = card.textContent.toLowerCase();
+
+        if (content.includes(searchText)) {
+            card.style.display = "";
+        } else {
+            card.style.display = "none";
+            searchResult.textContent = 'No search results found for "' + searchText + '"';
+        }
+    });
+}
