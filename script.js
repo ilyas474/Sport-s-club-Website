7292c4a1d621b895cd19b92736a3616eec788719
USER <thaiyangchua@gmail.com>
2026-09-26 01:24:26 +0800

    haha

--- script.js
+++ script.js
@@ -402,8 +402,7 @@ if (studentIdInput) {
     });
 }
 
-/* Gallery Page Image Zoom Function */
-document.addEventListener("DOMContentLoaded", function() ){
+document.addEventListener("DOMContentLoaded", function() {
     const filterButtons = document.querySelectorAll(".filter-btn");
     const galleryItems = document.querySelectorAll(".gallery-item");
 
@@ -444,37 +443,15 @@ document.addEventListener("DOMContentLoaded", function() ){
             }
         });
 
-        closeBtn.addEventListener("click",) () => {
+    closeBtn.addEventListener("click", () => {
+        lightbox.style.display = "none";
+    });
+
+    lightbox.addEventListener("click", (e) => {
+        if (e.target === lightbox) {
             lightbox.style.display = "none";
         }
-    };
-};
-
-/*rules*/
-document.addEventListener("DOMContentLoaded", function() {
-    const accHeaders = document.querySelectorAll(".accordion-header");
-
-    if (accHeaders.length > 0) {
-        accHeaders.forEach(header => {
-            header.addEventListener("click", function() {
-               
-                const content = this.nextElementSibling;
-                const isActive = this.classList.contains("active");
-                accHeaders.forEach(otherHeader => {
-                    otherHeader.classList.remove("active");
-                    otherHeader.nextElementSibling.style.maxHeight = null;
-                    otherHeader.nextElementSibling.style.paddingTop = "0";
-                    otherHeader.nextElementSibling.style.paddingBottom = "0";
-                });
-
-              
-                if (!isActive) {
-                    this.classList.add("active");
-                    content.style.maxHeight = content.scrollHeight + "px"; 
-                }
-            });
-        });
-    }
+    });
 });
 
 /*rules*/
